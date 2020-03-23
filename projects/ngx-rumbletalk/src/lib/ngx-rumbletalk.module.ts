e12900fd560f40f2a82e12851312a4dce876afef
Timothy John Fidelis <timothy.fidelis@gmail.com>
2020-03-24 01:56:41 +0800

    added forms module to the plugin

--- projects/ngx-rumbletalk/src/lib/ngx-rumbletalk.module.ts
+++ projects/ngx-rumbletalk/src/lib/ngx-rumbletalk.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { NgxRumbletalkComponent } from './ngx-rumbletalk.component';
 import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [NgxRumbletalkComponent],
-  imports: [HttpClientModule, BrowserModule],
+  imports: [HttpClientModule, BrowserModule, FormsModule],
   exports: [NgxRumbletalkComponent]
 })
 export class NgxRumbletalkModule {}
