b809ba539f07491434f1d323453d310a3177be93
Timothy John Fidelis <timothy.fidelis@gmail.com>
2020-03-26 01:43:03 +0800

    exported NgxRumbletalkService to be used outside of the library

--- projects/ngx-rumbletalk/src/lib/ngx-rumbletalk.module.ts
+++ projects/ngx-rumbletalk/src/lib/ngx-rumbletalk.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { NgxRumbletalkComponent } from './ngx-rumbletalk.component';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { NgxRumbletalkService } from './ngx-rumbletalk.service';
+import { NgxRumbletalkComponent } from './ngx-rumbletalk.component';
 
 @NgModule({
   declarations: [NgxRumbletalkComponent],
   imports: [HttpClientModule, BrowserModule, FormsModule],
-  exports: [NgxRumbletalkComponent]
+  exports: [NgxRumbletalkComponent, NgxRumbletalkService]
 })
 export class NgxRumbletalkModule {}
